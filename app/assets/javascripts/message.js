76c1fa241455fcfb98cb92f5c213ef82ffc2ef4e
motti322 <hashimotoryosuke322@icloud.com>
2020-07-31 12:01:51 +0900

    [WIP]自動更新機能の追加

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,7 +2,7 @@ $(function(){
   function buildHTML(message){
    if ( message.image ) {
      var html =
-      `<div class="message-box">
+      `<div class="message-box" data-message-id=${message.id}>
            <div class="message-box__name">
              ${message.user_name}
            </div>
@@ -17,7 +17,7 @@ $(function(){
      return html;
    } else {
      var html =
-      `<div class="message-box">
+      `<div class="message-box" data-message-id=${message.id}>
            <div class="message-box__name">
              ${message.user_name}
            </div>
@@ -53,5 +53,33 @@ $('#new_message').on('submit', function(e){
   .fail(function() {
     alert("メッセージ送信に失敗しました");
   })
+
   });
+
+  var reloadMessages = function() {
+    var last_message_id = $('.message-box:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      console.log(messages)
+      if (messages.length !== 0) {
+     var insertHTML = '';
+     $.each(messages, function(i, message) {
+       insertHTML += buildHTML(message)
+     });
+     $('.chat-main__message-list').append(insertHTML);
+     $('.chat-main__message-list').animate({ scrollTop: $('.chat-main__message-list')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+    if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
