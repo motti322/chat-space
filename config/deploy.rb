6589fc9f94bd31c22900563aae5da8621777ce44
motti322 <hashimotoryosuke322@icloud.com>
2020-07-31 14:53:33 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,7 +14,7 @@ set :rbenv_ruby, '2.5.1'
 
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['matsumotoryousuke@matsumotoryousukenoMacBook-Pro/ .ssh/ motti322.pem'] 
+                  keys: ['~/.ssh/motti322.pem'] 
 
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
